0230471d91614906c22e39fec17cbbec11831dfe
katiescavern <128101248+katiescavern@users.noreply.github.com>
2023-05-02 21:56:19 -0700

    Add files via upload

--- buttonSupport.mjs
+++ buttonSupport.mjs
@@ -1,10 +1,16 @@
 let x = 0;
 let y = 1;
+var catgirls = 0;
+var totalUwu = 0;
 function numberIncrease() {
     var numberElement = document.getElementById("number");
     x += y;
+    totalUwu += y;
     numberElement.textContent = x;
-    
+    if (catgirls > 0) {
+        var uwupersec = Math.floor(catgirls*(1+(totalUwu/10)));
+        upsElement.textContent = `UwUs per second: ${uwupersec}`;
+    }
 }
 function numberIncrease2() {
     var textElement = document.getElementById("text");
@@ -16,8 +22,39 @@ function numberIncrease2() {
         numberElement.textContent = x;
     } else {
         textElement.textContent = (`not enough uwus`);
-
     }
-    
-    
 }
+
+
+  function catgirlPurchase() {
+    var catgirlPrice = Math.floor((((catgirls * 2) * 700) / 3) + 100);
+    var catgirlElement = document.getElementById("catgirl");
+    catgirlElement.textContent = `catgirl (-${catgirlPrice})`;
+    var catCountElement = document.getElementById("kitty");
+    catCountElement.textContent = `number of catgirls: ${catgirls}`;
+    if (x > catgirlPrice){
+        x = x-catgirlPrice;
+        catgirls++;
+        catCountElement.textContent = `number of catgirls: ${catgirls}`;
+        var upsElement = document.getElementById("ups");
+        var uwupersec = math.Floor(catgirls*(1+(totalUwu/10)));
+        upsElement.textContent = `UwUs per second: ${uwupersec}`;
+    } else {
+        catCountElement.textContent = "you don't have enough uwus";
+    }
+}
+
+function toggleStyles() {
+    var stylesheet = document.getElementById("stylesheet");
+    if (stylesheet.getAttribute("href") == "pastel.css") {
+      stylesheet.setAttribute("href", "darkMode.css");
+    } else {
+      stylesheet.setAttribute("href", "pastel.css");
+    }
+  }
+
+
+
+setInterval(function() {
+    x += Math.floor(catgirls*(1+(totalUwu/10)));
+  }, 1000);
